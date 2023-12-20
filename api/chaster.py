55255b236d5bba3df743582570732c6af4ff2e93
swisi <swisi71@gmail.com>
2023-12-21 00:37:40 +0100

    prints entfernt

--- api/chaster.py
+++ api/chaster.py
@@ -2,13 +2,11 @@ import requests
 
 def get_user_profile(ca_username, client_id, client_secret):
 
-    print(ca_username, client_id, client_secret)
-
     url = f'https://api.chaster.app/users/profile/{ca_username}'
     headers = {
         'X-Chaster-Client-Id': client_id,
         'X-Chaster-Client-Secret': client_secret
     }
     response = requests.get(url, headers=headers)
-    print(response.json())
+
     return response.json()
