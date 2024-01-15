4558eecc03449c266ccd4022f8fb5ca44f516d67
swisi <swisi71@gmail.com>
2024-01-15 16:53:59 +0100

    db init angepasst

--- init_db.py
+++ init_db.py
@@ -1,6 +1,20 @@
+import os
+
 from app import app
 from database import db
 
+from flask_migrate import upgrade
+
+# Pfad zur Datenbankdatei
+database_path = './instance/database.sqlite'
+
 # Erstellen eines Anwendungskontextes
 with app.app_context():
-    db.create_all()
+    if not os.path.exists(database_path):
+        # Datenbank erstellen...
+        db.create_all()
+       
+    else:
+        # Datenbank existiert bereits, führe Migrationen durch
+        print("Führe Datenbank-Migrationen und Upgrades durch.")
+        upgrade()
