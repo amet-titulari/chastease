b2274f599a873950cf586218ddb441e4b93521f7
swisi <swisi71@gmail.com>
2024-01-17 09:34:08 +0000

    Journal DB Modell erstellt

--- journal/models.py
+++ journal/models.py
@@ -1,7 +1,23 @@
 #ca_extgension/models.py
 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from . import db
 
 class journal(db.Model):
-    # Ihre Modelldefinitionen hier
-    pass
+    journal_id = db.Column(db.Integer, primary_key=True)
+    boolesches_feld1 = db.Column(db.Boolean, default=False)
+    boolesches_feld2 = db.Column(db.Boolean, default=False)
+    boolesches_feld3 = db.Column(db.Boolean, default=False)
+    boolesches_feld4 = db.Column(db.Boolean, default=False)
+    freitextfeld = db.Column(db.String, nullable=True)
+
+    # Hinzufügen des createdate-Feldes
+    createdate = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Fremdschlüsselbeziehung zur Benutzertabelle
+    benutzer_id = db.Column(db.Integer, db.ForeignKey('benutzer.id'), nullable=False)
+    benutzer = db.relationship('Benutzer', backref=db.backref('lock_history', lazy=True))
+
+# Erstellen der Tabellen in der Datenbank
+db.create_all()
