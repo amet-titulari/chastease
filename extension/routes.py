868fa63e75549695b7b85da057deaf697e02de22
swisi <swisi71@gmail.com>
2024-01-18 14:51:48 +0000

    .

--- extension/routes.py
+++ extension/routes.py
@@ -1,76 +1,105 @@
-from flask import  request, jsonify, current_app, session
-
-
-from flask import render_template, flash
+from flask import  request, jsonify, current_app, session, render_template, redirect, url_for, flash
+from flask_login import login_user, current_user
 
 from . import extension
+from .models import db
+
+
+from benutzer.models import Benutzer
+
 from api.cahaster_extension import get_session_auth_info, get_session_info
 
 @extension.route('/')
 def index():
 
+    print(current_user.is_authenticated)
+
     content = f'    <div class="container">\
-                        <h1>Das ist die Erweiterung!!</h1>\
+                        <h1>Infos zur Erweiterungssession!</h1>\
                         <h3></h3>\
                         <p></p>\
                     </div>'
 
-    return render_template('extension/index.html', content=content) 
+    return redirect(url_for('extension.handle_token'))
+ 
 
 
 
 
 @extension.route('/handle_token', methods=['GET','POST'])
 def handle_token():
+
+
     if request.method == 'POST':
         data = request.json
         main_token = data.get('mainToken')
 
-        print(f'MainToken : {main_token}')
-
+        session['main_token'] = main_token
         sessionauth = get_session_auth_info(main_token)
 
-        #print(sessionauth)
-
+        # Extrahieren des 'data'-Teils
         if sessionauth['success']:
-            
-            sessionId               = sessionauth['data']['session']['sessionId']
-            benutzername            = sessionauth['data']['session']['lock']['user']['username']
-            benutzerId              = sessionauth['data']['session']['lock']['user']['_id']
-            lock_status             = sessionauth['data']['session']['lock']['status']
+            data = sessionauth['data']
 
-            print(f'SessionId: {sessionId} \nName: {benutzername} \nID {benutzerId} \nLock Status: {lock_status}')
+            sessionId  = sessionauth['data']['session']['sessionId']
+            username    = sessionauth['data']['session']['lock']['user']['username']
+            role        = sessionauth['data']['session']['lock']['user']['role']
+            avatarUrl  = sessionauth['data']['session']['lock']['user']['avatarUrl']
 
-            sessioninfo = get_session_info(sessionId)
-            #sprint(sessioninfo)
 
-            reasonsPreventingUnlocking = sessioninfo['data']['session']['lock']['reasonsPreventingUnlocking'] 
+        else:
+            print("Fehler: Die Antwort war nicht erfolgreich.")
 
-            for reasonNoUnlock in reasonsPreventingUnlocking:
-                print(reasonNoUnlock)
-                if reasonNoUnlock['reason'] == 'temporary_opening':
-                    flash(f'Das Schloss der {sessionId} ist Temporär geöffnet!')
-                else:
-                    flash(f'Das Schloss der {sessionId} ist VERSCHLOSSEN!')
 
-            content = {
-                'benutzername': 'MaxMustermann',
-                'benutzerId': 12345,
-                'lock_status': 'gesperrt',
-                'sessionId': 'abc123'
-            }
+        benutzer = Benutzer.query.filter_by(username=username).first()
+        
+        if not benutzer:
+            benutzer = Benutzer(username=username, role=role, avatarUrl=avatarUrl)
+            db.session.add(benutzer)
+            db.session.commit()
+            login_user(benutzer)    
 
-            print("Weiterleitung")      
-            return render_template('extension/session.html', content=content)
+        else: 
+            benutzer.username = username
+            benutzer.role = role
+            benutzer.avatarUrl = avatarUrl    
+            db.session.commit()
+            login_user(benutzer)  
+
+
+        if current_user.is_authenticated:
+            print("Anmeldung ok")
+        else:
+            print("Anmeldung nicht erfolgt")
+
+
+        sessioninfo = get_session_info(sessionId)
+
+        print(f'Current Username : {current_user.username} \nAvatar Url: {current_user.avatarUrl} \nRolle: {current_user.role}  ')
+
+        reasonsPreventingUnlocking = sessioninfo['data']['session']['lock']['reasonsPreventingUnlocking'] 
+
+        for reasonNoUnlock in reasonsPreventingUnlocking:
+            print(reasonNoUnlock)
+            if reasonNoUnlock['reason'] == 'temporary_opening':
+                flash(f'Das Schloss der {sessionId} ist Temporär geöffnet!')
+            else:
+                flash(f'Das Schloss der {sessionId} ist VERSCHLOSSEN!')
+
+      
+        print(current_user.__dict__)
+
+
+        print(f'Weiterleitung\n\n')      
+        return render_template('extension/session.html', content=current_user.__dict__)
     
     if request.method == 'GET':
-        
-        content = {
-            'benutzername': 'MaxMustermann',
-            'benutzerId': 12345,
-            'lock_status': 'gesperrt',
-            'sessionId': 'abc123'
-        }
+
+        content = {"Wert1":"Inhalt von Wert1"}
+
+        flash("Willkommen")
+
+
 
         return render_template('extension/session.html', content=content)
 
